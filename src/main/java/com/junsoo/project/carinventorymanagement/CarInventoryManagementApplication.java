3177ad0c4a673a61b81a59e6c1bc4e912f53e8e6
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-08-08 13:43:15 +0900

    Initial commit

---
+++ src/main/java/com/junsoo/project/carinventorymanagement/CarInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.junsoo.project.carinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInventoryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarInventoryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junsoo/project/carinventorymanagement/CarInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junsoo.project.carinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInventoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
